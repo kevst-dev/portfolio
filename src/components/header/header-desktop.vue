68e0e306ca9c12de8b76aafb4107888bccd5c9a4
kevst-dev <kevst-dev@protonmail.com>
2022-06-27 12:08:41 -0500

    Configurando la estructura del header-desktop y el logo.

--- src/components/header/header-desktop.vue
+++ src/components/header/header-desktop.vue
@@ -1,6 +1,9 @@
 <template>
   <header class="header-main">
-    hola header
+    <a class="header-main__logo">
+      &lt<span>Kevst</span>.dev/&gt
+    </a>
+    <div>navbar</div>
   </header>
 </template>
 
@@ -14,9 +17,18 @@
     padding: 0 95px;
 
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 
     background-color: var(--color-complemento);
+  }
+
+  .header-main__logo {
+    font-weight: 700;
+    color: var(--color-primario);
+  }
+  .header-main__logo span {
+    color: var(--color-texto-1);
   }
 </style>
