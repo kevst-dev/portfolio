a99552e5a935dd4761edef0e01ea17842e6eb41b
kevst-dev <kevst-dev@protonmail.com>
2022-07-27 12:32:23 -0500

    Creando un componente genérico para crear buttons y aplicándolo en section-home.

--- src/components/main/home/profile-information/profile-main.vue
+++ src/components/main/home/profile-information/profile-main.vue
@@ -1,24 +1,31 @@
 <template>
-  <div class="container-data">
+  <div class="container-profile">
     <Title/>
 
-    <p class="data-paragraph">
+    <p class="paragraph">
       Desarrollador WEB apasionado por la informática
       y el software libre, enfocado en nunca parar de aprender.
     </p>
 
     <SocialNetworks/>
 
-    <div class="data-buttons">
-      <a class="data-button" href="#">
-        <font-awesome-icon icon="fa-regular fa-envelope" size="lg" />
-        Contáctame
-      </a>
-
-      <a class="data-button--outline" href="#">
-        <font-awesome-icon icon="fa-regular fa-file-lines" />
-        Descargar CV
-      </a>
+    <div class="container-buttons">
+      <Button
+        name="Contáctame"
+        styleButton="button-normal"
+        urlIcon="fa-regular fa-envelope"
+      />
+      <Button
+        v-if="!isMobile()"
+        name="Descargar CV"
+        styleButton="button-outline"
+        urlIcon="fa-regular fa-file-lines"
+      />
+      <Button
+        v-else
+        name="Descargar CV"
+        styleButton="button-text"
+      />
     </div>
   </div>
 </template>
@@ -26,17 +33,26 @@
 <script>
   import Title from './title.vue'
   import SocialNetworks from './social-networks/social-main.vue'
+  import Button from '../../standard/button.vue'
 
   export default {
     components: {
       Title,
-      SocialNetworks
+      SocialNetworks,
+      Button
+    },
+    setup() {
+      const isMobile = () => { return (screen.width <= 420) }
+
+      return {
+        isMobile
+      }
     }
   }
 </script>
 
 <style scoped>
-  .container-data {
+  .container-profile {
     width: 50%;
     height: 436px;
 
@@ -51,112 +67,41 @@
     font-weight: 400;
   }
 
-  .data-buttons {
+  .container-buttons {
     display: flex;
     gap: 35px;
   }
 
-  .data-button {
-    cursor: pointer;
-    padding: 18px 32px;
-    border-radius: 10px;
-    border: 2px solid var(--color-primario);
-
-    font-size: var(--font-size-body-text);
-    font-weight: 600;
-
-    display: flex;
-    align-items: center;
-    gap: 10px;
-
-    color: var(--color-fondo);
-    background-color: var(--color-primario);
-  }
-  .data-button:hover {
-    background-color: var(--color-fondo);
-    color: var(--color-primario);
-  }
-
-  .data-button--outline {
-    cursor: pointer;
-    padding: 18px 32px;
-    border-radius: 10px;
-    border: 2px solid var(--color-primario);
-
-    font-size: var(--font-size-body-text);
-    font-weight: 600;
-
-    display: flex;
-    align-items: center;
-    gap: 10px;
-
-    color: var(--color-primario);
-  }
-  .data-button--outline:hover {
-    background-color: var(--color-primario);
-    color: var(--color-fondo);
-  }
-
-  /* Desktop-min */
-  @media screen and (max-width: 1256px){
-    .data-button {
-      font-size: var(--font-size-small);
-      font-weight: 600;
-    }
-    .data-button--outline {
-      font-size: var(--font-size-small);
-      font-weight: 600;
-    }
-  }
-
   /* Tablet */
   @media screen and (max-width: 768px){
-    .container-data {
+    .container-profile {
       width: 80%;
       justify-content: center;
       align-items: center;
       gap: 35px;
     }
 
-    .data-paragraph {
+    .paragraph {
       text-align: center;
     }
   }
 
   /* Tablet-min */
   @media screen and (max-width: 584px){
-    .container-data {
+    .container-profile {
       width: 100%;
     }
   }
 
   /* Mobile */
   @media screen and (max-width: 420px){
-    .container-data {
+    .container-profile {
       height: 100%;
     }
-    .data-buttons {
+    .container-buttons {
       flex-direction: column;
       gap: 0;
     }
-    .data-button, .data-button--outline {
-      width: 100%;
-      font-size: var(--font-size-min);
-    }
-    .data-button--outline {
-      border: 0px;
-      margin: 10px 0;
-      padding: 0;
-      color: var(--color-primario);
-      display: flex;
-      justify-content: center;
-    }
-    .data-button--outline:hover {
-      background-color: var(--color-fondo);
-      color: var(--color-primario);
-      text-decoration-line: underline;
-    }
-    .data-button--outline svg { display:none }
   }
 
 </style>
---
+++ src/components/main/standard/button.vue
@@ -0,0 +1,104 @@
+<template>
+  <a :class="styleButton" href="#">
+    <font-awesome-icon v-if="urlIcon != null" :icon="urlIcon" size="lg" />
+    {{name}}
+  </a>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      name: String,
+      styleButton: {
+        type: String,
+        default: "button-normal"
+      },
+      urlIcon: {
+        type: String,
+        default: null
+      },
+    },
+    components: {
+    },
+    setup() {
+    }
+  }
+</script>
+
+<style scoped>
+  .button-normal {
+    cursor: pointer;
+    padding: 18px 32px;
+    border-radius: 10px;
+    border: 2px solid var(--color-primario);
+
+    font-size: var(--font-size-body-text);
+    font-weight: 600;
+
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    color: var(--color-fondo);
+    background-color: var(--color-primario);
+  }
+  .button-normal:hover {
+    background-color: var(--color-fondo);
+    color: var(--color-primario);
+  }
+
+  .button-outline {
+    cursor: pointer;
+    padding: 18px 32px;
+    border-radius: 10px;
+    border: 2px solid var(--color-primario);
+
+    font-size: var(--font-size-body-text);
+    font-weight: 600;
+
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    color: var(--color-primario);
+  }
+  .button-outline:hover {
+    background-color: var(--color-primario);
+    color: var(--color-fondo);
+  }
+
+  .button-text {
+    font-size: var(--font-size-body-text);
+    font-weight: 600;
+    border: 0px;
+    margin: 10px 0;
+    padding: 0;
+    color: var(--color-primario);
+    display: flex;
+    justify-content: center;
+  }
+
+  .button-text:hover {
+    background-color: var(--color-fondo);
+    color: var(--color-primario);
+    text-decoration-line: underline;
+  }
+  /* Desktop-min */
+  @media screen and (max-width: 1256px){
+    .button-normal, .button-outline, .button-text {
+      font-size: var(--font-size-small);
+      font-weight: 600;
+    }
+  }
+
+  /* Mobile */
+  @media screen and (max-width: 420px){
+    .button-normal, .button-outline, .button-text {
+      width: 100%;
+      font-size: var(--font-size-min);
+    }
+  }
+
+
+</style>
